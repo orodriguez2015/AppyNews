36ecf9c90d55d42722b3b1ad00ed3ce17808adee
orodriguez2015 <oscar.rodriguezbrea@gmail.com>
2016-07-09 23:02:50 +0200

    Splash Screen

--- app/src/main/java/com/appynew/activities/SplashScreenActivity.java
+++ app/src/main/java/com/appynew/activities/SplashScreenActivity.java
@@ -16,7 +16,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     /**
      * Temporizador para la pantalla de bienvenida
      */
-    private static int SPLASH_TIEMPO = 1500;
+    private static int SPLASH_TIEMPO = 1000;
 
 
     /**
